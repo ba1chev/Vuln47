bf5558ec934cf30252e27fea430d8d7c6f38e7fe
ba1chev <baychevyoan@gmail.com>
2026-08-02 13:31:28 +0300

    Bundled the trainer and pipeline into the model_training namespace

---
+++ source/training/model_training/__init__.py
@@ -0,0 +1,6 @@
+from source.training.model_training.model_trainer import ModelTrainer
+from source.training.model_training.model_training_pipeline import ModelTrainingPipeline
+
+__all__ = [
+    "ModelTrainer", "ModelTrainingPipeline"
+]
