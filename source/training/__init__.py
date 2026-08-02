5d5bd2d8c63f3165c418b86a3ea7600c120a22d6
ba1chev <baychevyoan@gmail.com>
2026-08-02 13:31:02 +0300

    Exposed the four training abstractions at the package root

---
+++ source/training/__init__.py
@@ -0,0 +1,8 @@
+from source.training.trainer import Trainer
+from source.training.evaluator import Evaluator
+from source.training.batch_loader import BatchLoader
+from source.training.training_pipeline import TrainingPipeline
+
+__all__ = [
+    "Trainer", "Evaluator", "BatchLoader", "TrainingPipeline"
+]
