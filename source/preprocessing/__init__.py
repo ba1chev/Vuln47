b4891c97bd18b4724a66524dac88878ce8ea4d9b
ba1chev <baychevyoan@gmail.com>
2026-08-02 13:04:34 +0300

    Exposed abstract base classes in the preprocessing package init

---
+++ source/preprocessing/__init__.py
@@ -0,0 +1,7 @@
+from source.preprocessing.loader import Loader
+from source.preprocessing.domain_representator import DomainRepresentator
+from source.preprocessing.preprocessing_pipeline import PreprocessingPipeline
+
+__all__ = [
+    "Loader", "DomainRepresentator", "PreprocessingPipeline"
+]
