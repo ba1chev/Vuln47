5ea8646b7b1a59606f4b4c91432fe2060421cde4
ba1chev <baychevyoan@gmail.com>
2026-08-04 10:53:00 +0300

    Re-exported the Fetcher base from the preprocessing package init

--- source/preprocessing/__init__.py
+++ source/preprocessing/__init__.py
@@ -1,8 +1,9 @@
 from source.preprocessing.loader import Loader
+from source.preprocessing.fetcher import Fetcher
 from source.preprocessing.explorer import Explorer
 from source.preprocessing.domain_representator import DomainRepresentator
 from source.preprocessing.preprocessing_pipeline import PreprocessingPipeline
 
 __all__ = [
-    "Loader", "DomainRepresentator", "PreprocessingPipeline", "Explorer"
+    "Loader", "Fetcher", "DomainRepresentator", "PreprocessingPipeline", "Explorer"
 ]
