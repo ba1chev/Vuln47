e559b52634288755daa30f5f412ca9fce8e1e251
ba1chev <baychevyoan@gmail.com>
2026-08-10 14:42:54 +0300

    Explained how the BatchLoader turns a split into mini-batches

--- source/training/batch_loader.py
+++ source/training/batch_loader.py
@@ -6,6 +6,12 @@ BatchLoaderOutput = TypeVar("BatchLoaderOutput")
 
 
 class BatchLoader(ABC, Generic[BatchLoaderInput, BatchLoaderOutput]):
+    """Abstract base for turning a stored split into an iterable of mini-batches.
+
+    Concrete loaders decide how the raw split (e.g. a '.pt' path) is read and
+    grouped; 'load' returns a batch iterator the trainer/evaluator consume.
+    """
+
     @abstractmethod
     def load(self, input: BatchLoaderInput) -> BatchLoaderOutput:
         raise NotImplementedError("Must be implemented")
